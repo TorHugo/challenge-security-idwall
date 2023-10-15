b5512efeee1b3df0a9b14ec3bd4e87431068c88d
TorHugo <arruda.victorhugo@outlook.com>
2023-10-15 17:14:41 -0300

    FEAT (BASE_PROJECT): - Adding base project.

---
+++ src/main/java/com/dev/torhugo/challenge_idwall/ChallengeIdwallApplication.java
@@ -0,0 +1,13 @@
+package com.dev.torhugo.challenge_idwall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeIdwallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeIdwallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/torhugo/challenge_idwall/ChallengeIdwallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.torhugo.challenge_idwall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeIdwallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
